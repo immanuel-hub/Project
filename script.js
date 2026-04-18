eb48ebacd76edef939232cfef9118c170ecb2641
immanuel <immanuel.okoth@student.moringaschool.com>
2026-04-19 00:44:31 +0300

    edited all the forms both in action page and contact page by adding id to them so that they can be easily recognosed by javascript

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-
-const container = document.getElementById("items-container");
-
-function loadItems(){
-    fetch("https://dummyjson.com/products")
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data) {
-        displayItems(data.products);
-    });
-}
+/
